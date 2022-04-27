07a8e2e97be283a5e0eaa8adbc6d3214c92eb40a
Fedor <fed-sav@yandex.ru>
2022-04-28 01:26:16 +0300

    module 2 done

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
-function sumSalary(salaries) {
-  // ваш код...
+function sumSalary (salaries) {
+  
+  let sum = 0;
+  
+  for (key in salaries) {
+    if (typeof salaries[key] === 'number' && isFinite(salaries[key]) === true) {
+      sum += salaries[key]
+    }
+  }
+  return sum;
 }
