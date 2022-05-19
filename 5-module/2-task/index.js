4fa2f5ca0591fedea2d308dcedb03cde165f6ae5
Fedor <fed-sav@yandex.ru>
2022-05-19 20:39:23 +0300

    module 5 done

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,14 @@
 function toggleText() {
-  // ваш код...
+  let div = document.querySelector('#text');
+  let btn = document.querySelector('.toggle-text-button');
+
+  btn.addEventListener('click', (event) => {
+    if (event.target = div) {
+      if (div.hasAttribute('hidden')) {
+        div.removeAttribute('hidden')
+      } else {
+        div.setAttribute('hidden', true)
+      }
+    }
+  })
 }
